54407a03b2702181b9993d3311224755e017f75e
matiassalicru <mattiassalicru@gmail.com>
2022-09-19 16:44:00 -0300

    fix: env and fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel="preload"
-          as="font"
-          href='https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;700&display=swap'
-          type="font/woff2"
-          crossOrigin='true'
-        />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          rel="stylesheet"
+          as="font"
+          href='https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;700&display=swap'
+          crossOrigin='true'
+        />
       </Head>
       <body>
         <Main />
